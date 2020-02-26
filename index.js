a9cde13ccea18010d67b7318e8b7f67757d15d85
JustPhvn <justicequyphan@gmail.com>
2020-02-26 15:14:13 -0700

    moved stuff into api

--- index.js
+++ index.js
@@ -1,56 +1,73 @@
 const inquirer = require("inquirer");
+const api = require("./utils/api");
 
 const questions = [];
 
 function writeToFile(fileName, data) {}
 
 function init() {
-  inquirer.prompt([
-    {
-      type: "input",
-      name: "username",
-      message: "What is your GitHub username? "
-    },
-    {
-      type: "input",
-      name: "repo",
-      message: "What is your project name? "
-    },
-    {
-      type: "input",
-      name: "description",
-      message: "Please enter a description for your project: "
-    },
-    {
-      type: "list",
-      name: "license",
-      message: "What license should your project have? ",
-      choices: ["APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
-    },
-    {
-      type: "input",
-      name: "dependency",
-      message: "What command should be run to install dependencies? "
-    },
-    {
-      type: "input",
-      name: "test",
-      message: "What command should be run to run tests? "
-    },
-    {
-      type: "input",
-      name: "info",
-      message: "What does the user need to know about using the repo?"
-    },
-    {
-      type: "input",
-      name: "contribution",
-      message: "What does the user need to know about contributing to the repo?"
-    }
-  ]);
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "username",
+        message: "What is your GitHub username? "
+      }
+      // {
+      //   type: "input",
+      //   name: "repo",
+      //   message: "What is your project name? "
+      // },
+      // {
+      //   type: "input",
+      //   name: "description",
+      //   message: "Please enter a description for your project: "
+      // },
+      // {
+      //   type: "list",
+      //   name: "license",
+      //   message: "What license should your project have? ",
+      //   choices: ["APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+      // },
+      // {
+      //   type: "input",
+      //   name: "dependency",
+      //   message: "What command should be run to install dependencies? "
+      // },
+      // {
+      //   type: "input",
+      //   name: "test",
+      //   message: "What command should be run to run tests? "
+      // },
+      // {
+      //   type: "input",
+      //   name: "info",
+      //   message: "What does the user need to know about using the repo?"
+      // },
+      // {
+      //   type: "input",
+      //   name: "contribution",
+      //   message:
+      //     "What does the user need to know about contributing to the repo?"
+      // }
+    ])
+    .then(function({
+      username,
+      repo,
+      description,
+      license,
+      dependency,
+      test,
+      info,
+      contribution
+    }) {
+      api.getUser(username);
+    });
 }
 
 init();
+
+// `https://api.github.com/users/${username}?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`;
 
 //At least one badge
 // Project title
--- utils/api.js
+++ utils/api.js
@@ -1,6 +1,13 @@
-`https://api.github.com/users/${username}?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`;
+const axios = require("axios");
+
 const api = {
-  getUser(username) {}
+  getUser(username) {
+    axios
+      .get(
+        `https://api.github.com/users/${username}?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`
+      )
+      .then(({ data }) => console.log(data));
+  }
 };
 
 module.exports = api;
